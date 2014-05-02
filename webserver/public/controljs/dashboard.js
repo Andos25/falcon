1c779c63ffeaae031b65c2c67498eeb8e0871430
Andos25 <h.chujieandos@gmail.com>
2014-05-02 21:20:46 +0800

    dashboard finished

--- webserver/public/controljs/dashboard.js
+++ webserver/public/controljs/dashboard.js
@@ -50,6 +50,7 @@ $(document).ready(function(){
   function select_sexinfo(){
     $.getJSON("/ajax/dashboard_select_sexinfo/", {"selectinfo":$("select#multiSelect1 option:selected").attr("name")}, function(data) {
       buildsexchart(data);
+      console.log(data);
         }
     )}
   function buildsexchart(info) {
@@ -83,14 +84,10 @@ $(document).ready(function(){
         series: [{
             type: 'pie',
             name: '所占比例',
-            data: info
+            data: info.percentage
         }]
     });
-    categories = ['0-4', '5-9', '10-14', '15-19',
-            '20-24', '25-29', '30-34', '35-39', '40-44',
-            '45-49', '50-54', '55-59', '60-64', '65-69',
-            '70-74', '75-79', '80-84', '85-89', '90-94',
-            '95-99', '100 +'];
+    categories = info.categories;
     $('#sex_detail').highcharts({
             chart: {
                 type: 'bar'
@@ -123,13 +120,13 @@ $(document).ready(function(){
                 title: {
                     text: null
                 },
-                labels: {
-                    formatter: function(){
-                        return (Math.abs(this.value) / 1000000) + 'M';
-                    }
-                },
-                min: -4000000,
-                max: 4000000
+                // labels: {
+                //     formatter: function(){
+                //         return (Math.abs(this.value) / 1000000) + 'M';
+                //     }
+                // },
+                // min: -4000000,
+                // max: 4000000
             },
     
             plotOptions: {
@@ -147,14 +144,10 @@ $(document).ready(function(){
     
             series: [{
                 name: 'Male',
-                data: [-1746181, -1884428, -2089758, -2222362, -2537431, -2507081, -2443179,
-                    -2664537, -3556505, -3680231, -3143062, -2721122, -2229181, -2227768,
-                    -2176300, -1329968, -836804, -354784, -90569, -28367, -3878]
+                data: info.malelist
             }, {
                 name: 'Female',
-                data: [1656154, 1787564, 1981671, 2108575, 2403438, 2366003, 2301402, 2519874,
-                    3360596, 3493473, 3050775, 2759560, 2304444, 2426504, 2568938, 1785638,
-                    1447162, 1005011, 330870, 130632, 21208]
+                data: info.femalelist
             }]
         });
   };
--- webserver/routes/ajax.js
+++ webserver/routes/ajax.js
@@ -8,26 +8,46 @@ exports.select_sexinfo = function(req, res){
   mongoclient.open(function(err, mongoclient){
     var db = mongoclient.db("weibo");
     var collection = db.collection("provinces")
+    var result = {};
+    var malelist = new Array();
+    var femalelist = new Array();
+    var categories = new Array();
+    var percentage = new Array();
     if(req.query.selectinfo == 0){
-      collection.find().toArray(function(err, data){
-        var result = new Array();
+      collection.find().limit(15).toArray(function(err, data){
         var male = 0;
         var female = 0;
         for(var i in data){
           male += data[i]["m"];
           female += data[i]["f"];
+          malelist.push(0-data[i]["m"]);
+          femalelist.push(data[i]["f"]);
+          categories.push(data[i]["name"]);
         }
-        result.push(["male", male]);
-        result.push(["female", female]);
+        percentage.push(["male", male]);
+        percentage.push(["female", female]);
+        result["percentage"] = percentage;
+        result["categories"] = categories;
+        result["malelist"] = malelist;
+        result["femalelist"] = femalelist;
         mongoclient.close();
         res.json(result);
       });
     }else{
-      collection.find({"id":parseInt(req.query.selectinfo)}).toArray(function(err, data){
-        var result = new Array();
-        data = data[0];
-        result.push(["male", data["m"]]);
-        result.push(["female", data["f"]]);
+      collection.findOne({"id":parseInt(req.query.selectinfo)}, function(err, data){
+        percentage.push(["male", data["m"]]);
+        percentage.push(["female", data["f"]]);
+        data = data["citys"]
+        for(i in data){
+          malelist.push(0-data[i]["m"]);
+          femalelist.push(data[i]["f"]);
+          categories.push(data[i]["cname"]);
+        }
+
+        result["percentage"] = percentage;
+        result["categories"] = categories;
+        result["malelist"] = malelist;
+        result["femalelist"] = femalelist;
         mongoclient.close();
         res.json(result);
       });
@@ -43,10 +63,7 @@ exports.select_popinfo = function(req, res){
       collection.find().toArray(function(err, data){
         var result = new Array();
         for(var i in data){
-          var item = new Array();
-          item.push(data[i]["name"]);
-          item.push(data[i]["population"]);
-          result.push(item);
+          result.push([data[i]["name"], data[i]["population"]]);
         }
         mongoclient.close();
         res.json(result);
