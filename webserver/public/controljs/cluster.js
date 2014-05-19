5acc2233b7950e16f7a1328c999a7c226c6d6d2d
adiking <adikingustc@gmail.com>
2014-05-19 14:50:13 +0800

    k-means

---
+++ webserver/public/controljs/cluster.js
@@ -0,0 +1,60 @@
+$(document).ready(function() {
+  get_cluster_graph();
+});
+
+function get_cluster_graph() {
+  $.getJSON("/ajax/cluster_data/", {}, function(data) {
+    buildcolumnchart(data);
+  });
+}
+function buildcolumnchart(info) {
+     $('#container').highcharts({
+            chart: {
+                type: 'column',
+                margin: [ 50, 50, 100, 80]
+            },
+            title: {
+                text: 'World\'s largest cities per 2008'
+            },
+            xAxis: {
+                categories:info["categories"] ,
+                labels: {
+                    rotation: -45,
+                    align: 'right',
+                    style: {
+                        fontSize: '13px',
+                        fontFamily: 'Verdana, sans-serif'
+                    }
+                }
+            },
+            yAxis: {
+                min: 0,
+                title: {
+                    text: 'Population (millions)'
+                }
+            },
+            legend: {
+                enabled: false
+            },
+            tooltip: {
+                pointFormat: 'There are : <b>{point.y:.1f} blogs in this cluster</b>',
+            },
+            series: [{
+                name: 'Population',
+                data: info["data"],
+                dataLabels: {
+                    enabled: false,
+                    rotation: -90,
+                    color: '#FFFFFF',
+                    align: 'right',
+                    x: 4,
+                    y: 10,
+                    style: {
+                        fontSize: '13px',
+                        fontFamily: 'Verdana, sans-serif',
+                        textShadow: '0 0 3px black'
+                    }
+                }
+            }]
+        });
+}
